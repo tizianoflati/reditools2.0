8399b40b867e8a7b278fadb0b236804509c8c98c
tflati <tflati@di.uniroma1.it>
2017-11-08 17:53:44 +0100

    Improved extract coverage.sh

--- extract_coverage.sh
+++ extract_coverage.sh
@@ -1,12 +1,10 @@
-COVERAGE_DIR=$2
 FILENAME=`basename $1`
-COVERAGE_FILE=${FILENAME%.*}
-
-COVERAGE_DIR=$COVERAGE_DIR$COVERAGE_FILE"/"
+COVERAGE_DIR=$2
+FILE_ID=${FILENAME%.*}
 
 if [ ! -d $COVERAGE_DIR ]
 then
 	mkdir -p $COVERAGE_DIR
 fi
 
-samtools depth $1 | grep -vP "\t0$" | tee $COVERAGE_DIR$COVERAGE_FILE".cov" | awk '{print($0) > "'$COVERAGE_DIR'"$1}'
+samtools depth $1 | grep -vP "\t0$" | tee $COVERAGE_DIR$FILE_ID".cov" | awk '{print($0) > "'$COVERAGE_DIR'"$1}'
