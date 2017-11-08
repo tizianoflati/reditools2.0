157f1d83a6b60b203503d7bec8e4d8fbf66a5073
tflati <tflati@di.uniroma1.it>
2017-11-08 17:50:23 +0100

    Improved extract coverage.sh

--- extract_coverage.sh
+++ extract_coverage.sh
@@ -1,1 +1,12 @@
-samtools depth $1 | grep -vP "\t0$" | awk '{print $0 > "cov/"$1}'
+COVERAGE_DIR="cov/"
+FILENAME=`basename $1`
+COVERAGE_FILE=${FILENAME%.*}
+
+COVERAGE_DIR=$COVERAGE_DIR$COVERAGE_FILE"/"
+
+if [ ! -d $COVERAGE_DIR ]
+then
+	mkdir -p $COVERAGE_DIR
+fi
+
+samtools depth $1 | grep -vP "\t0$" | tee $COVERAGE_DIR$COVERAGE_FILE".cov" | awk '{print($0) > "'$COVERAGE_DIR'"$1}'
