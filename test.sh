c011d33f56884fcae2fedfb9f36a1d9e539093c6
tflati <tflati@di.uniroma1.it>
2018-02-15 12:01:08 +0100

    Added test data

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 INPUT_BAM_FILE="test/SRR2135332.bam"
-python src/cineca/reditools.py -f $INPUT_BAM_FILE -o table.txt -g chr1
+python src/cineca/reditools.py -f $INPUT_BAM_FILE -o table.txt -s 1 -g chr1
