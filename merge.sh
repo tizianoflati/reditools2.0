52b0ded29751d47fcb11e6d4e8a63e27d8074f59
tflati <tflati@di.uniroma1.it>
2018-02-28 13:07:58 +0100

    Added merge procedure

---
+++ merge.sh
@@ -0,0 +1,28 @@
+TABLE_DIR=$1
+FINAL_TABLE=$2
+THREADS=$3
+
+if [ ! -s $TABLE_DIR/files.txt ]
+then
+    echo "FILE LIST NOT EXISTING OR EMPTY: "$TABLE_DIR/files.txt
+else
+    OUTPUT_DIR=`dirname $FINAL_TABLE`
+    if [ ! -d $OUTPUT_DIR ]
+    then
+        mkdir -p $OUTPUT_DIR
+    fi
+    
+    zcat $(cat $TABLE_DIR/files.txt) | bgzip -d -c -@ $THREADS > $FINAL_TABLE
+    echo "Finished creating final table $FINAL_TABLE ["`date`"]"
+fi
+
+# echo "Starting creating final table "`date`
+# 
+# i=0; for file in $(ls $TABLE_DIR/*.gz)
+# do
+#     i=$((i + 1))
+#     echo $i". "$file" "`date`; zcat $file >> final_file.txt
+# done
+# 
+# echo "Compressing final_file.txt "`date`
+# time /marconi/home/userexternal/tflati00/pigz-2.4/pigz -c final_file.txt > final_file.gz
