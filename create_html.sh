071a435a5016b3d556dddb21b3d77ee379c332b1
Tiziano Flati <tflati00@r000u06l01.marconi.cineca.it>
2018-09-13 12:00:30 +0200

    Fixed serial test and generation of HTML template

--- create_html.sh
+++ create_html.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 TEMPDIR=$1
-cat template.html | sed "s@EVENTS_DATA@$(cat "$1"/times.txt)@g" | sed "s@GROUPS_DATA@$(cat "$1"/groups.txt)@g" > reditools.html
+#cat template.html | sed "s@EVENTS_DATA@$(cat "$1"/times.txt)@g" | sed "s@GROUPS_DATA@$(cat "$1"/groups.txt)@g" > reditools.html
+cat template.html | sed "/EVENTS_DATA/{s/EVENTS_DATA//g
+r "$1"/times.txt
+}" | sed "/GROUPS_DATA/{s/GROUPS_DATA//g
+r "$1"/groups.txt
+}" > reditools.html
--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
+module load profile/global
+module load ig_homo_sapiens/hg19
+
 INPUT_BAM_FILE="test/SRR2135332.bam"
 python src/cineca/reditools.py -f $INPUT_BAM_FILE -r $IG_HG19_GENOME/genome.fa -o table.txt -s 1 -g chr1
